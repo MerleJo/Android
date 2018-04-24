fed0515e90d7eb4033c0fe37c5ba69fd94935c63
Konsalor <Stifff-Daniel@web.de>
2018-04-24 14:10:07 +0300

    Test

--- app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
+++ app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class PizzaOrder extends Activity {
 
+    String      fattyMcFatfett;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
