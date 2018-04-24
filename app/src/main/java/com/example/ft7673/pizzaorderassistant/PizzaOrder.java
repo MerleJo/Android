957dc1d920bf3dbe9b74f29b613704dcbaf1f1a8
MerleJo <36482746+MerleJo@users.noreply.github.com>
2018-04-24 14:12:45 +0300

    as
    AS

--- app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
+++ app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
@@ -2,12 +2,19 @@ package com.example.ft7673.pizzaorderassistant;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PizzaOrder extends Activity {
 
+    TextView test;
+    TextView test2;
+
+    TextView testjetzt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
