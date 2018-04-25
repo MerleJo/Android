07d0c14ee52e05c5a8a7bf0a5d8c4bd621b18a9e
MerleJo <36482746+MerleJo@users.noreply.github.com>
2018-04-25 14:44:57 +0300

    Stand MI 15 Uhr

--- app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
+++ app/src/main/java/com/example/ft7673/pizzaorderassistant/PizzaOrder.java
@@ -1,17 +1,21 @@
 package com.example.ft7673.pizzaorderassistant;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class PizzaOrder extends Activity implements RadioGroup.OnCheckedChangeListener {
+public class PizzaOrder extends Activity implements RadioGroup.OnCheckedChangeListener, View.OnClickListener, DialogInterface.OnClickListener {
 
 
     RadioGroup  rdGroup;
@@ -21,6 +25,8 @@ public class PizzaOrder extends Activity implements RadioGroup.OnCheckedChangeLi
     Button      btnOrderOK;
 
     private int orderType = 0;          // 1= Pizzeria, 2=Takeaway, 3=Delivery
+
+    AlertDialog alertSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,10 +74,16 @@ public class PizzaOrder extends Activity implements RadioGroup.OnCheckedChangeLi
                 break;
         }
     }
-    public void onClick(Button buttonView){
-        switch(buttonView.getId()){
+
+    public void onClick(View v){            // funktioniert nicht
+        switch(v.getId()){
             case R.id.btnOrderTypeOK:
-                setContentView(R.layout.activity_main);
+                if(rdGroup.getCheckedRadioButtonId() != -1){
+                    setContentView(R.layout.activity_main);
+                }else{
+                    Toast.makeText(this,R.string.toastSelectType, Toast.LENGTH_LONG).show();
+                }
+
                 break;
             case R.id.btnDeliveryOK:
                 // Intent für neue activity_send schreiben
@@ -87,8 +99,28 @@ public class PizzaOrder extends Activity implements RadioGroup.OnCheckedChangeLi
                     case 3:
                         setContentView(R.layout.delivery_info);
                         break;
+                    default:
+
                 }
                 break;
         }
+    }
+
+    private AlertDialog selectSize(){
+        AlertDialog.Builder diabuilder = new AlertDialog.Builder(this);
+        diabuilder.setTitle(R.string.alertSizeTitle)
+               // .setIcon()        noch einfügen
+
+        .setSingleChoiceItems(R.array.arraySizes, -1, new DialogInterface.OnClickListener() {
+
+        });
+
+        alertSize = diabuilder.create();
+
+        return alertSize;
+    }
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
