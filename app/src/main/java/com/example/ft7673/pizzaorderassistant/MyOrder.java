1be7e3abc0053579bc1158dfefd935d2da6596a2
Konsalor <Stifff-Daniel@web.de>
2018-04-29 11:27:36 +0300

    Intent repariert

--- app/src/main/java/com/example/ft7673/pizzaorderassistant/MyOrder.java
+++ app/src/main/java/com/example/ft7673/pizzaorderassistant/MyOrder.java
@@ -4,13 +4,18 @@ package com.example.ft7673.pizzaorderassistant;
 import java.io.Serializable;
 
 public class MyOrder implements Serializable{
-    private double total;
+    private double total = 0.00;
     private Pizza[] order;
 
     public MyOrder(Pizza[] order){
         this.order = order;
         for(int i = 0; i < order.length; i++){
-           this.total = this.total + order[i].getPizzaPrice();
+            if(order[i] == null){
+                break;
+            }
+            else {
+                this.total = this.total + order[i].getPizzaPrice();
+            }
         }
     }
     public double getTotal(){
